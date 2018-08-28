c24808fed00c4159c5648b3e8662e3e782e431ac
Mitch Ryan <mitch.ryan@outlook.co.nz>
2018-08-28 22:51:23 +1200

    Remove redundant raf polyfill from test environment

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import 'raf/polyfill';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 /* eslint-enable import/no-extraneous-dependencies */
